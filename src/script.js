84f1d1587ccc3302d8af52829d405bb20bffcc54
fredysomy <fredysomy@gmail.com>
2021-04-25 09:09:15 +0000

    added movement

--- src/script.js
+++ src/script.js
@@ -11,32 +11,141 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+
 const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
 light.position.set(0.5, 1, 0.75);
 scene.add(light);
 
-const controls = new PointerLockControls(camera, document.body);
-const blocker = document.getElementById("blocker");
-const instructions = document.getElementById("instructions");
-const btnn = document.getElementById("btn");
-btnn.addEventListener("click", function () {
-  var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-  if (isMobile) {
-    document.getElementById("instructions").style.display = "none";
-    document.getElementById("blocker").style.display = "none";
+  const controls = new PointerLockControls(camera, document.body);
+  const blocker = document.getElementById("blocker");
+  const instructions = document.getElementById("instructions");
+  const btnn = document.getElementById("btn");
+  btnn.addEventListener("click", function () {
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      document.getElementById("instructions").style.display = "none";
+      document.getElementById("blocker").style.display = "none";
+    }
+    controls.lock();
+  });
+  controls.addEventListener("lock", function () {
+    instructions.style.display = "none";
+    blocker.style.display = "none";
+  });
+
+  controls.addEventListener("unlock", function () {
+    blocker.style.display = "block";
+    instructions.style.display = "";
+  });
+  scene.add(controls.getObject());
+
+
+const onKeyDown = function (event) {
+  switch (event.keyCode) {
+    case 38:
+    case 87:
+      moveForward = true;
+      break;
+
+    case 37:
+    case 65:
+      moveLeft = true;
+      break;
+
+    case 40:
+    case 83:
+      moveBackward = true;
+      break;
+
+    case 39:
+    case 68:
+      moveRight = true;
+      break;
+
+    case 32:
+      if (canJump === true) velocity.y += 350;
+      canJump = false;
+      break;
   }
-  controls.lock();
+};
+
+const onKeyUp = function (event) {
+  switch (event.keyCode) {
+    case 38:
+    case 87:
+      moveForward = false;
+      break;
+
+    case 37:
+    case 65:
+      moveLeft = false;
+      break;
+
+    case 40:
+    case 83:
+      moveBackward = false;
+      break;
+
+    case 39:
+    case 68:
+      moveRight = false;
+      break;
+  }
+};
+
+const forward = document.getElementById("controller-forward");
+forward.addEventListener("touchstart", function () {
+  console.log("touch");
+  moveForward = true;
 });
-controls.addEventListener("lock", function () {
-  instructions.style.display = "none";
-  blocker.style.display = "none";
+forward.addEventListener("touchend", function () {
+  console.log("end");
+  moveForward = false;
+});
+forward.addEventListener("touchmove", function () {
+  console.log("end");
+  moveForward = false;
 });
 
-controls.addEventListener("unlock", function () {
-  blocker.style.display = "block";
-  instructions.style.display = "";
+const right = document.getElementById("controller-right");
+right.addEventListener("touchstart", function () {
+  moveRight = true;
 });
-scene.add(controls.getObject());
+right.addEventListener("touchend", function () {
+  moveRight = false;
+});
+
+const left = document.getElementById("controller-left");
+left.addEventListener("touchstart", function () {
+  moveLeft = true;
+});
+left.addEventListener("touchend", function () {
+  moveLeft = false;
+});
+
+const back = document.getElementById("controller-back");
+back.addEventListener("touchstart", function () {
+  moveBackward = true;
+});
+back.addEventListener("touchend", function () {
+  moveBackward = false;
+});
+
+document.addEventListener("touchmove", function () {
+  console.log("moved");
+});
+document.addEventListener("keydown", onKeyDown, false);
+document.addEventListener("keyup", onKeyUp, false);
+
 camera.position.set(0, 10, 180);
 const renderer = new THREE.WebGLRenderer({
   alpha: true,
@@ -44,7 +153,7 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
 const plane = new THREE.PlaneGeometry(2000, 2000, 100, 100);
 const materialPlane = new THREE.MeshBasicMaterial({
@@ -61,12 +170,44 @@ const BoxgeoMetry = new THREE.Mesh(box, materialBox);
 scene.add(BoxgeoMetry);
 BoxgeoMetry.position.set(0, 0, 0);
 
-scene.add(controls.getObject());
 
-controls.moveRight(0);
 
 const animate = function () {
   requestAnimationFrame(animate);
+
+  const time = performance.now();
+
+
+
+
+
+
+  const delta = (time - prevTime) / 1000;
+
+  velocity.x -= velocity.x * 10.0 * delta;
+  velocity.z -= velocity.z * 8.0 * delta;
+  velocity.y -= 0 * delta; 
+  direction.z = Number(moveForward);
+  direction.x = Number(moveRight) - Number(moveLeft);
+  direction.normalize(); 
+
+  if (moveForward) velocity.z -= direction.z * 400.0 * delta;
+  if (moveBackward) velocity.z += 400.0 * delta;
+  if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+
+ 
+
+  controls.moveRight(-velocity.x * delta);
+  controls.moveForward(-velocity.z * delta);
+
+  controls.getObject().position.y += velocity.y * delta;
+  if (controls.getObject().position.y < 10) {
+    velocity.y = 0;
+    controls.getObject().position.y = 10;
+    canJump = true;
+  }
+
+  prevTime = time;
 
   renderer.render(scene, camera);
 };
